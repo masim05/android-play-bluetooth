271ed4db33cc472c06f34ad13abe1f2328bfa496
Max Ryndin <rgmasim@gmail.com>
2021-10-30 20:14:39 +0300

    Add button with scanning.

--- app/src/main/java/com/example/playwithbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/playwithbluetooth/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.playwithbluetooth
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
+
+private const val TAG = "Main Activity"
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.v(TAG, "On create started")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+
+
+        val button: Button = findViewById(R.id.scan_button)
+        button.setOnClickListener {
+            val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+            if (bluetoothAdapter == null) {
+                Log.e(TAG, "bluetoothAdapter is null")
+            }
+        }
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device?.name
+                    val deviceHardwareAddress = device?.address // MAC address
+                    Log.v(TAG, "$deviceName: $deviceHardwareAddress")
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver)
     }
 }
+
